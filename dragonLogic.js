25bf9f2fd9426db969e787e0dfcbc9bd60f8302b
oliver-cameron <94218640+oliver-cameron@users.noreply.github.com>
2022-09-15 16:29:04 +1000

    Dragonrealm v3.3

--- dragonLogic.js
+++ dragonLogic.js
@@ -7,7 +7,7 @@ var modal = document.getElementById("myModal");
 var hpb = document.getElementById("healthBar");
 var aub = document.getElementById("goldBar");
 var span = document.getElementsByClassName("close")[0];
-var keydown = "null";
+let keydown = [];
 var hp = 50;
 var au = 0;
 var set = "null"
@@ -37,42 +37,52 @@ document.addEventListener('keydown', function (event) {
     if (keydown == "ArrowRight"){
         play(2);
     }
-    if (keydown == "Meta"){
-        console.log(Date.now())
+    if (keydown == "˘"){
+        keydown = [];
+        hp = 50;
+        au = 0;
+        set = "null"
+        times = [];
+        angels = 0;
+        pickaxe = 0;
+        guessCount = 0;
     }
 });
 
 function play(guess) {
-    if (guessCount == 0){
+    if (set == "null"){
+        if (guessCount == 0){
+            secret = Math.floor(Math.random() * 2) + 1;
+            guessCount = 1;
+        }
+        
+        console.log()
+        if (guess != 1 && guess != 2) {
+            document.getElementById("msg_gold").style.display = "none";
+            document.getElementById("msg_die").style.display = "none";
+            document.getElementById("msg_error").style.display = "block";
+        } else if (guess == secret) {
+            document.getElementById("msg_gold").style.display = "block";
+            document.getElementById("msg_die").style.display = "none";
+            document.getElementById("msg_error").style.display = "none";
+            timesAdd(1000,"msg_gold_delete");
+            au += getRandomInt(1, 6);
+        } else {
+            document.getElementById("msg_gold").style.display = "none";
+            document.getElementById("msg_die").style.display = "block";
+            document.getElementById("msg_error").style.display = "none";
+            timesAdd(1000,"msg_die_delete");
+            hp -= getRandomInt(1, 6);
+        }
+       
+    
         secret = Math.floor(Math.random() * 2) + 1;
-        guessCount = 1;
+        if (angels !== 0){
+            console.log ("Choose "+ secret);
+            angels --;
+        } 
     }
     
-    console.log()
-    if (guess != 1 && guess != 2) {
-        document.getElementById("msg_gold").style.display = "none";
-        document.getElementById("msg_die").style.display = "none";
-        document.getElementById("msg_error").style.display = "block";
-    } else if (guess == secret) {
-        document.getElementById("msg_gold").style.display = "block";
-        document.getElementById("msg_die").style.display = "none";
-        document.getElementById("msg_error").style.display = "none";
-        timesAdd(1000,"msg_gold_delete");
-        au += getRandomInt(1, 6);
-    } else {
-        document.getElementById("msg_gold").style.display = "none";
-        document.getElementById("msg_die").style.display = "block";
-        document.getElementById("msg_error").style.display = "none";
-        timesAdd(1000,"msg_die_delete");
-        hp -= getRandomInt(1, 6);
-    }
-   
-
-    secret = Math.floor(Math.random() * 2) + 1;
-    if (angels !== 0){
-        console.log ("Choose "+ secret);
-        angels --;
-    } 
 }
 
 function getRandomInt(min, max) {
@@ -82,118 +92,120 @@ function getRandomInt(min, max) {
 }
 
 function product(num) {
-    if (num == 1 ) {
-        if (au >= 2){
-                au -= 2;
-                hp += 1;
+    if (set == "null"){
+        if (num == 1 ) {
+            if (au >= 2){
+                    au -= 2;
+                    hp += 1;
+                } else {
+                    document.getElementById("msg_tooExpensive").style.display = "block";
+                    timesAdd(1000,"msg_tooExpensive_delete");
+                } 
+        }
+        if (num == 2) {
+            if(au >= 3){
+                au -= 3;
+                hp += 2;
             } else {
                 document.getElementById("msg_tooExpensive").style.display = "block";
                 timesAdd(1000,"msg_tooExpensive_delete");
-            } 
-    }
-    if (num == 2) {
-        if(au >= 3){
-            au -= 3;
-            hp += 2;
-        } else {
-            document.getElementById("msg_tooExpensive").style.display = "block";
-            timesAdd(1000,"msg_tooExpensive_delete");
-        }   
-    }
-    if (num == 3) {
-        if(au >= 5)
-        {   au -= 5;
-            times = times.concat([
-            { instance: Date.now() + 1000, instruction: "hpAdd1" },
-            { instance: Date.now() + 3000, instruction: "hpAdd1" },
-            { instance: Date.now() + 5000, instruction: "hpAdd1" },
-            { instance: Date.now() + 7000, instruction: "hpAdd1" },
-            { instance: Date.now() + 9000, instruction: "hpAdd1" },
-            { instance: Date.now() + 11000, instruction: "hpAdd1" },
-            { instance: Date.now() + 13000, instruction: "hpAdd1" },
-            { instance: Date.now() + 15000, instruction: "hpAdd1" },
-            { instance: Date.now() + 17000, instruction: "hpAdd1" },
-            { instance: Date.now() + 19000, instruction: "hpAdd1" },
-            { instance: Date.now() + 21000, instruction: "hpAdd1" },
-            ]);} else{
+            }   
+        }
+        if (num == 3) {
+            if(au >= 5)
+            {   au -= 5;
+                times = times.concat([
+                { instance: Date.now() + 1000, instruction: "hpAdd1" },
+                { instance: Date.now() + 3000, instruction: "hpAdd1" },
+                { instance: Date.now() + 5000, instruction: "hpAdd1" },
+                { instance: Date.now() + 7000, instruction: "hpAdd1" },
+                { instance: Date.now() + 9000, instruction: "hpAdd1" },
+                { instance: Date.now() + 11000, instruction: "hpAdd1" },
+                { instance: Date.now() + 13000, instruction: "hpAdd1" },
+                { instance: Date.now() + 15000, instruction: "hpAdd1" },
+                { instance: Date.now() + 17000, instruction: "hpAdd1" },
+                { instance: Date.now() + 19000, instruction: "hpAdd1" },
+                { instance: Date.now() + 21000, instruction: "hpAdd1" },
+                ]);} else{
+                    document.getElementById("msg_tooExpensive").style.display = "block";
+                    timesAdd(1000,"msg_tooExpensive_delete");
+                }
+            
+        }
+        if (num == 4){
+            if(au >= 30){
+                au -= 30;
+                times = times.concat([
+                { instance: Date.now() + 1000, instruction: "hpAdd2" },
+                { instance: Date.now() + 2000, instruction: "hpAdd2" },
+                { instance: Date.now() + 3000, instruction: "hpAdd2" },
+                { instance: Date.now() + 4000, instruction: "hpAdd2" },
+                { instance: Date.now() + 5000, instruction: "hpAdd2" },
+                { instance: Date.now() + 6000, instruction: "hpAdd2" },
+                { instance: Date.now() + 7000, instruction: "hpAdd2" },
+                { instance: Date.now() + 8000, instruction: "hpAdd2" },
+                { instance: Date.now() + 9000, instruction: "hpAdd2" },
+                { instance: Date.now() + 10000, instruction: "hpAdd2" },
+                { instance: Date.now() + 11000, instruction: "hpAdd2" },
+                { instance: Date.now() + 12000, instruction: "hpAdd2" },
+                { instance: Date.now() + 13000, instruction: "hpAdd2" },
+                { instance: Date.now() + 14000, instruction: "hpAdd2" },
+                { instance: Date.now() + 15000, instruction: "hpAdd2" },
+                { instance: Date.now() + 16000, instruction: "hpAdd2" },
+                { instance: Date.now() + 17000, instruction: "hpAdd2" },
+                { instance: Date.now() + 18000, instruction: "hpAdd2" },
+                { instance: Date.now() + 19000, instruction: "hpAdd2" },
+                { instance: Date.now() + 20000, instruction: "hpAdd2" },
+                { instance: Date.now() + 21000, instruction: "hpAdd2" },
+                { instance: Date.now() + 1000, instruction: "auAdd2" },
+                { instance: Date.now() + 2000, instruction: "auAdd2" },
+                { instance: Date.now() + 3000, instruction: "auAdd2" },
+                { instance: Date.now() + 4000, instruction: "auAdd2" },
+                { instance: Date.now() + 5000, instruction: "auAdd2" },
+                { instance: Date.now() + 6000, instruction: "auAdd2" },
+                { instance: Date.now() + 7000, instruction: "auAdd2" },
+                { instance: Date.now() + 8000, instruction: "auAdd2" },
+                { instance: Date.now() + 9000, instruction: "auAdd2" },
+                { instance: Date.now() + 10000, instruction: "auAdd2" },
+                { instance: Date.now() + 11000, instruction: "auAdd2" }, 
+                { instance: Date.now() + 12000, instruction: "auAdd2" },
+                { instance: Date.now() + 13000, instruction: "auAdd2" },
+                { instance: Date.now() + 14000, instruction: "auAdd2" },
+                { instance: Date.now() + 15000, instruction: "auAdd2" },
+                { instance: Date.now() + 16000, instruction: "auAdd2" },
+                { instance: Date.now() + 17000, instruction: "auAdd2" },
+                { instance: Date.now() + 18000, instruction: "auAdd2" },
+                { instance: Date.now() + 19000, instruction: "auAdd2" },
+                { instance: Date.now() + 20000, instruction: "auAdd2" },
+                { instance: Date.now() + 21000, instruction: "auAdd2" },
+                ]);
+            } else{
                 document.getElementById("msg_tooExpensive").style.display = "block";
                 timesAdd(1000,"msg_tooExpensive_delete");
             }
-        
-    }
-    if (num == 4){
-        if(au >= 30){
-            au -= 30;
-            times = times.concat([
-            { instance: Date.now() + 1000, instruction: "hpAdd2" },
-            { instance: Date.now() + 2000, instruction: "hpAdd2" },
-            { instance: Date.now() + 3000, instruction: "hpAdd2" },
-            { instance: Date.now() + 4000, instruction: "hpAdd2" },
-            { instance: Date.now() + 5000, instruction: "hpAdd2" },
-            { instance: Date.now() + 6000, instruction: "hpAdd2" },
-            { instance: Date.now() + 7000, instruction: "hpAdd2" },
-            { instance: Date.now() + 8000, instruction: "hpAdd2" },
-            { instance: Date.now() + 9000, instruction: "hpAdd2" },
-            { instance: Date.now() + 10000, instruction: "hpAdd2" },
-            { instance: Date.now() + 11000, instruction: "hpAdd2" },
-            { instance: Date.now() + 12000, instruction: "hpAdd2" },
-            { instance: Date.now() + 13000, instruction: "hpAdd2" },
-            { instance: Date.now() + 14000, instruction: "hpAdd2" },
-            { instance: Date.now() + 15000, instruction: "hpAdd2" },
-            { instance: Date.now() + 16000, instruction: "hpAdd2" },
-            { instance: Date.now() + 17000, instruction: "hpAdd2" },
-            { instance: Date.now() + 18000, instruction: "hpAdd2" },
-            { instance: Date.now() + 19000, instruction: "hpAdd2" },
-            { instance: Date.now() + 20000, instruction: "hpAdd2" },
-            { instance: Date.now() + 21000, instruction: "hpAdd2" },
-            { instance: Date.now() + 1000, instruction: "auAdd2" },
-            { instance: Date.now() + 2000, instruction: "auAdd2" },
-            { instance: Date.now() + 3000, instruction: "auAdd2" },
-            { instance: Date.now() + 4000, instruction: "auAdd2" },
-            { instance: Date.now() + 5000, instruction: "auAdd2" },
-            { instance: Date.now() + 6000, instruction: "auAdd2" },
-            { instance: Date.now() + 7000, instruction: "auAdd2" },
-            { instance: Date.now() + 8000, instruction: "auAdd2" },
-            { instance: Date.now() + 9000, instruction: "auAdd2" },
-            { instance: Date.now() + 10000, instruction: "auAdd2" },
-            { instance: Date.now() + 11000, instruction: "auAdd2" }, 
-            { instance: Date.now() + 12000, instruction: "auAdd2" },
-            { instance: Date.now() + 13000, instruction: "auAdd2" },
-            { instance: Date.now() + 14000, instruction: "auAdd2" },
-            { instance: Date.now() + 15000, instruction: "auAdd2" },
-            { instance: Date.now() + 16000, instruction: "auAdd2" },
-            { instance: Date.now() + 17000, instruction: "auAdd2" },
-            { instance: Date.now() + 18000, instruction: "auAdd2" },
-            { instance: Date.now() + 19000, instruction: "auAdd2" },
-            { instance: Date.now() + 20000, instruction: "auAdd2" },
-            { instance: Date.now() + 21000, instruction: "auAdd2" },
-            ]);
-        } else{
-            document.getElementById("msg_tooExpensive").style.display = "block";
-            timesAdd(1000,"msg_tooExpensive_delete");
-        }
-        
-    } if (num == 5){
-        if (au >= 15){
-            if (angels == 0){
-                angels = 5
-                au -= 15;
+            
+        } if (num == 5){
+            if (au >= 15){
+                if (angels == 0){
+                    angels = 5
+                    au -= 15;
+                } else {
+                    console.log("You already have an angel.")
+                }
             } else {
-                console.log("You already have an angel.")
+                document.getElementById("msg_tooExpensive").style.display = "block";
+                timesAdd(1000,"msg_tooExpensive_delete");
             }
-        } else {
-            document.getElementById("msg_tooExpensive").style.display = "block";
-            timesAdd(1000,"msg_tooExpensive_delete");
+            
+        } if (num == 6){
+            if(au >= 5){
+                au -= 5;
+            pickaxe = 1000000;
+            } else{
+                document.getElementById("msg_tooExpensive").style.display = "block";
+                timesAdd(1000,"msg_tooExpensive_delete");
+            }  
         }
-        
-    } if (num == 6){
-        if(au >= 5){
-            au -= 5;
-        pickaxe = 1000000;
-        } else{
-            document.getElementById("msg_tooExpensive").style.display = "block";
-            timesAdd(1000,"msg_tooExpensive_delete");
-        }  
     }
 }
 function timesAdd(mili,instruct){
@@ -226,25 +238,26 @@ setInterval(function () {
         document.getElementById("msg_win").style.display = "block";
         timesAdd(1000,"msg_win_delete")
     }
-    console.log("Time");
-    if (pickaxe !== 0){
-        if (hp <= 0 && set !== "win") {
-            set = "lose"
-            document.getElementById("msg_lose").style.display = "block";
-        } else if (au >= 50 && set !== "lose") {
-            set = "win"
-            document.getElementById("msg_win").style.display = "block";
-            console.log("YOU WON");
-        }
-        if (getRandomInt(1,100001) = 1){
-            if (getRandomInt(1,11)== 1){
-                au = 50;
-            } else {
-                au++;
-                console.log("You struck it rich!");
+    if (set == "null"){
+        if (pickaxe !== 0){
+            if (hp <= 0 && set !== "win") {
+                set = "lose"
+                document.getElementById("msg_lose").style.display = "block";
+            } else if (au >= 50 && set !== "lose") {
+                set = "win"
+                document.getElementById("msg_win").style.display = "block";
+                console.log("YOU WON");
             }
+            if (getRandomInt(1,100001) = 1){
+                if (getRandomInt(1,11)== 1){
+                    au = 50;
+                } else {
+                    au++;
+                    console.log("You struck it rich!");
+                }
+            }
+            pickaxe --;
         }
-        pickaxe --;
     }
     for (var i = 0; i < times.length; i++) {
         if (Date.now() >= times[i].instance) {
